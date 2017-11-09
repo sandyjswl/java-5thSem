07c4c42debb759ac22aa36df7e1b020ce88658fc
sandyjswl <sandeepjswl123@gmail.com>
2017-11-09 23:20:25 +0530

    Deleted class files

--- Day 7 - Interfaces/Interfaces3.java
+++ Day 7 - Interfaces/Interfaces3.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import java.util.Scanner;
 
 
@@ -30,7 +30,7 @@ class EducationLoan extends Loan
 	{
 		System.out.println("Simple Interest is: "+calculateInterest());
 	}
-	
+
 
 }
 
@@ -48,7 +48,7 @@ class HomeLoan extends Loan
 	{
 		System.out.println("Compound Interest is: "+calculateInterest());
 	}
-	
+
 
 }
 class Interfaces3
@@ -63,95 +63,15 @@ class Interfaces3
 		double t=sc.nextDouble();
 
 		EducationLoan loan1 = new EducationLoan(p,r,t);
-		loan=loan1;		
+		loan=loan1;
 		loan.display();
 
 		HomeLoan loan2=new HomeLoan(p,r,t);
 		loan=loan2;
-		loan.display(); 
-		
-
-	}
-
-
-}
-
-=======
-import java.util.Scanner;
-
-
-abstract class Loan
-{
-	double loanAmount,interestRate,years;
-	abstract double calculateInterest();
-	abstract void display();
-	Loan(double p, double r, double t)
-	{
-		loanAmount=p;
-		interestRate=r;
-		years=t;
-	}
-
-}
-
-class EducationLoan extends Loan
-{
-	EducationLoan(double p, double r, double t)
-	{
-		super(p,r,t);
-	}
-	double calculateInterest()
-	{
-		return ((loanAmount*interestRate*years)/100);
-	}
-	void display()
-	{
-		System.out.println("Simple Interest is: "+calculateInterest());
-	}
-	
-
-}
-
-class HomeLoan extends Loan
-{
-	HomeLoan(double p, double r, double t)
-	{
-		super(p,r,t);
-	}
-	double calculateInterest()
-	{
-		return (loanAmount*Math.pow((1+(interestRate/100)),years));
-	}
-	void display()
-	{
-		System.out.println("Compound Interest is: "+calculateInterest());
-	}
-	
-
-}
-class Interfaces3
-{
-	public static void main(String args[])
-	{
-		Loan loan;
-		System.out.println("ENter the Loan Amount, Interest Rate and No of Years: ");
-		Scanner sc =new Scanner(System.in);
-		double p=sc.nextDouble();
-		double r= sc.nextDouble();
-		double t=sc.nextDouble();
-
-		EducationLoan loan1 = new EducationLoan(p,r,t);
-		loan=loan1;		
 		loan.display();
 
-		HomeLoan loan2=new HomeLoan(p,r,t);
-		loan=loan2;
-		loan.display(); 
-		
 
 	}
 
 
 }
-
->>>>>>> origin/master
--- Day 8 - Packages/Question 2/ComplexNumbers.java
+++ Day 8 - Packages/Question 2/ComplexNumbers.java
@@ -3,13 +3,13 @@ class ComplexNumbers
 {
 	public static void main(String args[])
 	{
-		System.out.print("First Complex Number=");
+		System.out.print("\n\nFirst Complex Number=");
 		ComplexNumber a=new ComplexNumber(5,10);
 		a.display();
-		System.out.print("\nSecond Complex Number=");
+		System.out.print("\n\nSecond Complex Number=");
 		ComplexNumber b=new ComplexNumber(10,15);
 		b.display();
-		System.out.print("\nSum of Complex Numbers=");
+		System.out.print("\n\nSum of Complex Numbers=");
 		b=b.addComplex(a);
 		b.display();
 
